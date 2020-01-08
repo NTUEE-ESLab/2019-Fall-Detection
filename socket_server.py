1fada0822af15b6473e5b6a005a026a73733dca3
CantorC <53324543+CantorC@users.noreply.github.com>
2020-01-08 20:58:27 +0800

    Add files via upload

--- socket_server.py
+++ socket_server.py
@@ -7,9 +7,12 @@ import json
 import time
 import random
 import pickle
+import math
 import os
+import csv
+import sys
 
-HOST = '192.168.5.111'  # Standard loopback interface address (localhost)
+HOST = '192.168.1.237'  # Standard loopback interface address (localhost)
 PORT = 65431            # Port to listen on (non-privileged ports are > 1023)
 
 sample = 0
@@ -35,21 +38,35 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print('Connected by', addr)
         a = 0
-        accel_arr = np.zeros((1000,3))
-        while a < 1000:
+        #accel_arr = np.zeros((1000,3))
+        #abs_acc = np.zeros(1000)
+        while True:
             data = conn.recv(1024)
             dec = data.decode()
+            movement_list = ["predict","fall","run","walk","still"]
+            print(movement_list[int(dec)])
+            """
             if (dec!=""):
                 x = dec.split(",")[:-1]
-                print("x=",x)
+                #print(x)
                 n_data = int(len(x)/3)
                 for b in range(n_data):
                     accel_arr[a] = np.array(x[3*b:3*(b+1)])
+                    sq_sum = np.sum(accel_arr[a]**2)
+                    print(math.sqrt(sq_sum))
+                    abs_acc[a] = math.sqrt(sq_sum)
                     a = a+1
-                    print(x[3*(b-1):3*b])
+                    #print(x[3*(b-1):3*b])
                 #print(dec)
-        with open('accel_arr', 'wb') as f:
+            """
+        """
+        with open('accel_arr_'+sys.argv[1], 'wb') as f:
             pickle.dump(accel_arr,f)
             #print(type(data))
             #conn.sendall(get_graph_data().encode('utf-8'))
 
+        with open('abs_acc_'+sys.argv[1]+'.csv', 'w', newline='', encoding='utf-8') as wf:
+            writer = csv.writer(wf)
+            for index in range (1000):
+                writer.writerow([abs_acc[index]])
+        """
